66fab5ceac920c149b5fc9cedbdbca83d5ce2397
Harsh <harsh.miglani010@gmail.com>
2023-07-28 18:19:34 +0530

    portfolio

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from '../assets/img/nav-icon4.png';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+            <a href="https://www.linkedin.com/in/harsh-miglani-0627321b8"><img src={navIcon1} alt="" /></a>
+            <a href="https://github.com/HarshMiglani"><img src={navIcon4} alt="" /></a>
+            <a href="https://instagram.com/harsh_miglani?igshid=MzNlNGNkZWQ4Mg=="><img src={navIcon3} alt="" /></a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
