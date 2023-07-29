6f9b612a5ccc87f87dd1b8f7ebf9c6aa38628134
Harsh <harsh.miglani010@gmail.com>
2023-07-29 16:02:43 +0530

    css changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/logo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 import navIcon4 from '../assets/img/nav-icon4.png';
